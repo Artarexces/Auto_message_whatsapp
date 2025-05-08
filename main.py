768f718d5edfc56ade25f7abe21e84f58869a7fc
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-05-07 22:41:26 -0300

    arquitectura final & txt.example

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time 
 import pywhatkit as kit 
 
-archivo_contacto = "contactos.txt" 
+archivo_contacto = "contactos_ejemplo.txt" 
 
 mensaje_base = "Hola {nombre}, te voy a romper el orto con la comision."
 
@@ -12,3 +12,14 @@ hora_actual = time.localtime()
 minuto = (hora_actual.tm_min + 2) % 60
 hora_extra = (hora_actual.tm_min +2) // 60
 hora_envio = (hora_actual.tm_hour + hora_extra, minuto) 
+
+for i, (nombre, numero) in enumerate(contactos):
+    mensaje_personalizado = mensaje_base.format(nombre=nombre) 
+
+    envio_hora = hora_envio[0] + ((hora_envio[1] + i)// 60)
+    envio_minuto = (hora_envio[1]+i) % 60
+
+print(f"Enviado a {nombre} ({numero}) a las {hora_envio}:{envio_minuto}...")
+
+kit.sendwhatmsg(numero, mensaje_personalizado, envio_hora, envio_minuto)
+
