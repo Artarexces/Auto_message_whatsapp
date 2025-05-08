a3f4c252933e714b88649286f855bd49fdea566d
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-05-08 20:58:08 -0300

    v02

--- main-test.py
+++ main-test.py
@@ -1,6 +1,11 @@
 import time 
 import pywhatkit as kit
 
+nombre = "atzel"
+nombre = nombre.upper()
+
 hora = time.localtime()
 hora_envio = (hora.tm_hour,(hora.tm_min + 2) %60)
-kit.sendwhatmsg("+5491171038515", "Traniella te saluda desde python",hora_envio[0], hora_envio[1])
+kit.sendwhatmsg("+5491160155806", f"Hola {nombre} ahora si funciona esta poha ?",hora_envio[0], hora_envio[1]) 
+
+
--- main.py
+++ main.py
@@ -14,12 +14,10 @@ hora_extra = (hora_actual.tm_min +2) // 60
 hora_envio = (hora_actual.tm_hour + hora_extra, minuto) 
 
 for i, (nombre, numero) in enumerate(contactos):
-    mensaje_personalizado = mensaje_base.format(nombre=nombre) 
-
+    mensaje_personalizado = mensaje_base.format(nombre=nombre)
     envio_hora = hora_envio[0] + ((hora_envio[1] + i)// 60)
     envio_minuto = (hora_envio[1]+i) % 60
 
 print(f"Enviado a {nombre} ({numero}) a las {hora_envio}:{envio_minuto}...")
 
 kit.sendwhatmsg(numero, mensaje_personalizado, envio_hora, envio_minuto)
-
