7f256035894e9a9bd0aba595f13f4568071ef04a
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-05-08 23:27:58 -0300

    finish structure

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ archivo_contacto = "contactos_ejemplo.txt"
 
 mensaje_base = "Hola {nombre}, te voy a romper el orto con la comision."
 
-with open(archivo_contacto, "r", encoding="utf-8") as archivo:
-    contactos = [linea.strip().split(",")for linea in archivo if linea.strip()]
+with open (archivo_contacto, "r", encoding="utf-8") as archivo:
+    contactos = [linea.strip().split(",") for linea in archivo if linea.strip()]
 
 hora_actual = time.localtime()
 minuto = (hora_actual.tm_min + 2) % 60
@@ -17,7 +17,7 @@ for i, (nombre, numero) in enumerate(contactos):
     mensaje_personalizado = mensaje_base.format(nombre=nombre)
     envio_hora = hora_envio[0] + ((hora_envio[1] + i)// 60)
     envio_minuto = (hora_envio[1]+i) % 60
-
-print(f"Enviado a {nombre} ({numero}) a las {hora_envio}:{envio_minuto}...")
-
-kit.sendwhatmsg(numero, mensaje_personalizado, envio_hora, envio_minuto)
+    print(f"Enviado a {nombre} ({numero}) a las {hora_envio}:{envio_minuto}...")
+    kit.sendwhatmsg_instantly(numero, mensaje_personalizado, wait_time=10, tab_close=True,close_time=3)
+    
+time.sleep(5)
